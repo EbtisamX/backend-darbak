5f303abfe9eeb83d4e1421b66bbccfa0913ba99b
EbtisamX <eebtisam58@gmail.com>
2025-04-30 12:57:38 +0300

    add Job application model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+APPLICATION_STATUS_CHOICES = (
+    ('waiting', 'Waiting'),
+    ('interview', 'Interview Scheduled'),
+    ('accepted', 'Accepted'),
+    ('rejected', 'Rejected'),
+)
+
+class JobApplication(models.Model):
+    company_name = models.CharField(max_length=100)
+    job_title = models.CharField(max_length=100)
+    application_date = models.DateField()
+    application_status = models.CharField(
+        max_length=20,
+        choices=APPLICATION_STATUS_CHOICES,
+        default='waiting'
+    )
+
+    def str(self):
+        return f"{self.job_title} at {self.company_name}"
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,4 @@
-from django.urls import path
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
 
-@api_view(['GET'])
-def hello_world(request):
-    return Response({"message": "Helloo, world!"})
-urlpatterns = [
-    path('api/hello/', hello_world),
-]
+from django.urls import path
+
+urlpatterns = []

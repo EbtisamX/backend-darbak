a5777cf68fc4309df14c01da037343682709d428
EbtisamX <eebtisam58@gmail.com>
2025-04-30 15:39:26 +0300

    add job serializer + creat jobListCreat in view file

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,4 +1,6 @@
-
 from django.urls import path
+from .views import JobApplicationListCreate
 
-urlpatterns = []
+urlpatterns = [
+    path('jobs/', JobApplicationListCreate.as_view(), name='job-list-create'),
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import JobApplication
+from .serializers import JobApplicationSerializer
 
-# Create your views here.
+class JobApplicationListCreate(APIView):
+    def get(self, request):
+        jobs = JobApplication.objects.all()
+        serializer = JobApplicationSerializer(jobs, many=True)
+        return Response(serializer.data, status=200)

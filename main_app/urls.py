e84a38ae505fbf4f4b3e0cd5896bcd3e4a08f5cf
EbtisamX <eebtisam58@gmail.com>
2025-05-03 01:08:50 +0300

    add path url for NoteDetailView

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import JobApplicationListCreate,JobApplicationDetailView,NoteListCreate
+from .views import JobApplicationListCreate,JobApplicationDetailView,NoteListCreate,NoteDetailView
 
 urlpatterns = [
     path('jobs/', JobApplicationListCreate.as_view(), name='job-list-create'),
     path('jobs/<int:pk>/', JobApplicationDetailView.as_view(), name='job-detail'),
     path('notes/', NoteListCreate.as_view(), name='note-list-create'),
-    
-
+    path('notes/<int:pk>/', NoteDetailView.as_view(), name='note-detail'),
 ]
